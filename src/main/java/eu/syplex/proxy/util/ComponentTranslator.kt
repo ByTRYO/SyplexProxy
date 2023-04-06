23d547ba24e41ecf52caf0f59ea7dff0557608ed
MerryDev <svgr1808@outlook.de>
2023-04-06 15:12:11 +0200

    Changed to colors and implementation of placeholder support

--- src/main/java/eu/syplex/proxy/util/ComponentTranslator.kt
+++ src/main/java/eu/syplex/proxy/util/ComponentTranslator.kt
@@ -1,11 +1,24 @@
 package eu.syplex.proxy.util
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import ninja.leaping.configurate.ConfigurationNode
 
 class ComponentTranslator(private val configurationNode: ConfigurationNode) {
+
     fun fromConfig(node: String, child: String): Component {
-        val content = configurationNode.getNode(node).getNode(child).getString(null)
-        return Component.text(content)
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(raw(node, child))
     }
+
+    fun fromConfigWithReplacement(node: String, child: String, placeholder: String, value: String): Component {
+        val raw = raw(node, child)
+        val replaced = raw.replace(placeholder, value)
+
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(replaced)
+    }
+
+    private fun raw(node: String, child: String): String {
+        return configurationNode.getNode(node, child).getString(null)
+    }
+
 }
