89430774038c543631c0dfffb9899761176ef8a1
ByTRYO <ByTRYO@gmail.com>
2023-04-13 14:23:49 +0200

    Implemented news and joinMe

--- src/main/java/eu/syplex/proxy/ProxyPlugin.kt
+++ src/main/java/eu/syplex/proxy/ProxyPlugin.kt
@@ -15,6 +15,9 @@ import eu.syplex.proxy.config.ConfigLoader
 import eu.syplex.proxy.config.PropertyLoader
 import eu.syplex.proxy.util.CommandPool
 import eu.syplex.proxy.util.ComponentTranslator
+import eu.syplex.proxy.util.JoinMeValidator
+import eu.syplex.proxy.util.news.NewsFetcher
+import eu.syplex.proxy.util.news.NewsLoader
 import ninja.leaping.configurate.ConfigurationNode
 import java.nio.file.Path
 import java.util.logging.Logger
@@ -25,6 +28,9 @@ class ProxyPlugin @Inject constructor(val proxyServer: ProxyServer, logger: Logg
     private val configurationNode: ConfigurationNode = ConfigLoader(dataDirectory).configurationNode
     private val translator: ComponentTranslator = ComponentTranslator(configurationNode)
     private val playerTracker: PlayerTracker = PlayerTracker()
+    private val validator : JoinMeValidator = JoinMeValidator(this, proxyServer, translator)
+    private val newsLoader : ConfigurationNode = NewsLoader(dataDirectory).configurationNode
+    private val newsFetcher : NewsFetcher = NewsFetcher(newsLoader)
 
     @Subscribe
     fun onProxyInitialization(event: ProxyInitializeEvent) {
@@ -43,6 +49,9 @@ class ProxyPlugin @Inject constructor(val proxyServer: ProxyServer, logger: Logg
         pool.register(MuteCommand(translator, proxyServer, playerTracker, configurationNode), "mute")
         pool.register(UnmuteCommand(translator, playerTracker), "unmute")
         pool.register(TeamChatCommand(proxyServer, translator), "teamchat", "tc")
+        pool.register(JoinMeCommand(translator, validator, proxyServer), "joinme")
+        pool.register(JoinMeAcceptCommand(translator, validator), "accept")
+        pool.register(NewsCommand(translator, newsFetcher), "news")
     }
 
     private fun registerListener() {
