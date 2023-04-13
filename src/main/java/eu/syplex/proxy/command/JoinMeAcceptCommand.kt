d62dda6b565aa69dadb173091d79149685c611aa
ByTRYO <ByTRYO@gmail.com>
2023-04-13 14:03:58 +0200

    Add joinme feature

---
+++ src/main/java/eu/syplex/proxy/command/JoinMeAcceptCommand.kt
@@ -0,0 +1,35 @@
+package eu.syplex.proxy.command
+
+import com.velocitypowered.api.command.SimpleCommand
+import com.velocitypowered.api.proxy.Player
+import eu.cloudnetservice.modules.bridge.player.PlayerManager
+import eu.syplex.proxy.util.ComponentTranslator
+import eu.syplex.proxy.util.JoinMeValidator
+import eu.syplex.proxy.util.Placeholder
+
+
+class JoinMeAcceptCommand(private val translator: ComponentTranslator, private val validator: JoinMeValidator) : SimpleCommand {
+    override fun execute(invocation: SimpleCommand.Invocation) {
+        val sender = invocation.source()
+        val args = invocation.arguments()
+
+        if (sender !is Player) {
+            sender.sendMessage(translator.fromConfig("no-player"))
+            return
+        }
+
+        if (args.isNotEmpty()) {
+            sender.sendMessage(translator.fromConfigWithReplacement("invalid-usage", Placeholder.command, "accept"))
+            return
+        }
+
+        if (!validator.isCooldownActive()) {
+            sender.sendMessage(translator.fromConfig("joinme-not-active"))
+            return
+        }
+
+        val manager = eu.cloudnetservice.driver.registry.ServiceRegistry.first(PlayerManager::class.java) ?: return
+        manager.onlinePlayer(sender.uniqueId)?.playerExecutor()?.connect(validator.getServer().serverInfo.name)
+
+    }
+}
