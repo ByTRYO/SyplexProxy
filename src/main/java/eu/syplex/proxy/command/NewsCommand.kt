49dba8bd766c9da096be57f82d8fd0cfff551c10
ByTRYO <ByTRYO@gmail.com>
2023-04-13 13:39:17 +0200

    Added message sending

--- src/main/java/eu/syplex/proxy/command/NewsCommand.kt
+++ src/main/java/eu/syplex/proxy/command/NewsCommand.kt
@@ -3,9 +3,10 @@ package eu.syplex.proxy.command
 import com.velocitypowered.api.command.SimpleCommand
 import com.velocitypowered.api.proxy.Player
 import eu.syplex.proxy.util.ComponentTranslator
+import eu.syplex.proxy.util.news.NewsFetcher
 
 
-class NewsCommand(private val translator: ComponentTranslator) : SimpleCommand {
+class NewsCommand(private val translator: ComponentTranslator, private val news: NewsFetcher) : SimpleCommand {
     override fun execute(invocation: SimpleCommand.Invocation) {
         val sender = invocation.source()
         val args = invocation.arguments()
@@ -18,6 +19,11 @@ class NewsCommand(private val translator: ComponentTranslator) : SimpleCommand {
         if (args.isNotEmpty()) {
             sender.sendMessage(translator.fromConfig("invalid-usage"))
             return
+        }
+
+        val list = news.getNews()
+        list.forEach { str ->
+            sender.sendMessage(str)
         }
 
     }
