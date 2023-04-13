408cae6515ae5a56a91c3d8a0d9686c672622916
ByTRYO <ByTRYO@gmail.com>
2023-04-13 13:41:07 +0200

    Add news-loading support

--- src/main/java/eu/syplex/proxy/ProxyPlugin.kt
+++ src/main/java/eu/syplex/proxy/ProxyPlugin.kt
@@ -10,16 +10,12 @@ import com.velocitypowered.api.proxy.ProxyServer
 import eu.syplex.proxy.backend.PlayerTracker
 import eu.syplex.proxy.backend.database.sadu.StaticDataLoader
 import eu.syplex.proxy.backend.listener.PostLoginListener
-import eu.syplex.proxy.command.JoinMeAcceptCommand
-import eu.syplex.proxy.command.JoinMeCommand
-import eu.syplex.proxy.command.PingCommand
-import eu.syplex.proxy.command.YouTubeCommand
+import eu.syplex.proxy.command.*
 import eu.syplex.proxy.config.ConfigLoader
 import eu.syplex.proxy.config.PropertyLoader
-import eu.syplex.proxy.util.CommandPool
-import eu.syplex.proxy.util.ComponentTranslator
-import eu.syplex.proxy.util.JoinMeValidator
-import eu.syplex.proxy.util.Notifier
+import eu.syplex.proxy.util.*
+import eu.syplex.proxy.util.news.NewsFetcher
+import eu.syplex.proxy.util.news.NewsLoader
 import ninja.leaping.configurate.ConfigurationNode
 import java.nio.file.Path
 import java.util.logging.Logger
@@ -28,6 +24,8 @@ import java.util.logging.Logger
 class ProxyPlugin @Inject constructor(val proxyServer: ProxyServer, logger: Logger, @DataDirectory private val dataDirectory: Path) {
 
     private val configurationNode: ConfigurationNode = ConfigLoader(dataDirectory).configurationNode
+    private val newsLoader : ConfigurationNode = NewsLoader(dataDirectory).configurationNode
+    private val newsFetcher : NewsFetcher = NewsFetcher(newsLoader)
     private val translator: ComponentTranslator = ComponentTranslator(configurationNode)
     private val notifier: Notifier = Notifier(proxyServer, translator)
     private val playerTracker: PlayerTracker = PlayerTracker()
@@ -46,6 +44,7 @@ class ProxyPlugin @Inject constructor(val proxyServer: ProxyServer, logger: Logg
         pool.register(YouTubeCommand(translator), "youtube", "yt")
         pool.register(JoinMeCommand(translator, joinMeValidator, proxyServer), "joinme")
         pool.register(JoinMeAcceptCommand(translator, joinMeValidator), "accept")
+        pool.register(NewsCommand(translator, newsFetcher), "news")
     }
 
     private fun registerListener() {
